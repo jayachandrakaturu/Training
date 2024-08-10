dd1dce00498bef06495efb31d3e584d765eec8c6
jayachandrakaturu <31104112+jayachandrakaturu@users.noreply.github.com>
2024-08-10 21:54:54 +0530

    10/08/24

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,50 @@
-// alert('Welcome to javascript file');
+var person = [];
+console.log('test1');
+console.log('test2');
+console.log(person.length);
+console.log('test3');
+console.log('test4');
+
+/** 10/08/24 */
+
+var person = 'Jaya Chandra'; /* var */
+let person2 = 'jaya chandra katuru'; /* let */
+const person3 = 'Suresh'; /** const */
+person = 'test1';
+
+/**DATA TYPES */
+var age = 22; /**Number data type */
+var userName1 = 'Jaya Chandra'  /**String data type */
+var isEmployee = true /**boolean */
+var nullData = null; /**null */
+var undefined;  /**undefined */
+
+var car = {
+    name: 'BMW',
+    cost: 2000,
+    oldModel: true
+} /**Object */
+
+var collection = [22, 'jaya', true, null, undefined, { name: 'test' }, [26, 29, 39]]; /**array */
+console.log(age, userName1, isEmployee, nullData, undefined, car, collection);
+
+
+
+// 1. JavaScript Object oriented programing 
+// 2. JavaScript dom interactions 
+
+// 1.1 JavaScript functions, and object 
+// 1.2 JavaScript DOM events 
+
+
+// 3. JavaScript Variable are to store the data and it will  return the data when we need
+// 4. JavaScript Variable type 
+//    Var, let, const 
+
+//    var is global scope 
+//    let is an block scope 
+//    const no value can changes 
+
+
+// 5. Data types in JavaScript
+// 5.1 Number, String, Boolean, null, undefined, Object, array
